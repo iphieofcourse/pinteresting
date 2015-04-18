4c00a9fef85d65786b3c39259bd5eb74e1de586f
Morgan Schultz <morgan.i.schultz@gmail.com>
2015-04-18 15:57:46 -0500

    Added S3 for Paperclip uploads on Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,13 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { :host => 'https://morning-woodland-6881.herokuapp.com/'}
+
+  config.paperclip_defaults = {
+      :storage => :s3,
+      :s3_credentials => {
+          :bucket => ENV['iphinteresting'],
+          :access_key_id => ENV['AKIAI6GXT3ITJZUHBUBQ'],
+          :secret_access_key => ENV['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']
+      }
+  }
 end
