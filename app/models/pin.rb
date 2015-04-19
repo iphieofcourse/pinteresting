e64340aeefb648c8e17416a54b25c8282aae10d3
Morgan Schultz <morgan.i.schultz@gmail.com>
2015-04-18 22:26:42 -0500

    fixed pin.rb to use s3

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,9 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
 
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+	has_attached_file :image, :style => { :medium => "300x300>", :thumb => "100x100>" },
+			:storage => :s3,
+			:bucket => ENV['iphinteresting']
+	validates :image, presence: true
+	validates :description, presence: true
 end
